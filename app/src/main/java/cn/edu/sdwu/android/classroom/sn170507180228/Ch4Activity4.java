b0fe1edb146605ae54a50975b452d4fcd71ae76e
wangyang1998916 <2931113553@qq.com>
2020-04-07 17:11:11 +0800

    更新

--- app/src/main/java/cn/edu/sdwu/android/classroom/sn170507180228/Ch4Activity4.java
+++ app/src/main/java/cn/edu/sdwu/android/classroom/sn170507180228/Ch4Activity4.java
@@ -1,6 +1,12 @@
 package cn.edu.sdwu.android.classroom.sn170507180228;
 
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.content.DialogInterface;
+import android.provider.Settings;
+import android.support.v4.app.NotificationBuilderWithBuilderAccessor;
 import android.support.v7.app.ActionBar;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Gravity;
@@ -38,6 +44,51 @@ public class Ch4Activity4 extends AppCompatActivity {
         toast.setGravity(Gravity.CENTER,0,0);
         toast.show();
 
+
+    }
+    public  void notification(View view){
+        Notification.Builder builder=new Notification.Builder(this);
+        builder.setContentTitle("title");
+        builder.setContentText("message");
+        builder.setWhen(System.currentTimeMillis());
+        builder.setSmallIcon(android.R.drawable.ic_input_delete);//引用系统的资源
+        //调用build方法完成构造
+       Notification notification= builder.build();
+        //使用NotificationManager，发送通知
+        NotificationManager notificationManager=(NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+        notificationManager.notify(101,notification);
+    }
+    public void cancel_notification(View view){
+        //使用NotificationManager取消通知
+        NotificationManager notificationManager=(NotificationManager)getSystemService(NOTIFICATION_SERVICE);
+        notificationManager.cancel(101);
+    }
+    public  void  alert_dialog(View view){
+        AlertDialog.Builder builder=new AlertDialog.Builder(this);
+        //在构造器中设置对话框的属性
+        builder.setTitle("title");
+        builder.setMessage("message");
+        builder.setIcon(android.R.drawable.ic_dialog_email);
+        builder.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                //添加点击按钮后的处理
+                Toast.makeText(Ch4Activity4.this,"ok",Toast.LENGTH_SHORT).show();
+            }
+        });
+        builder.setNegativeButton("exit", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                Toast.makeText(Ch4Activity4.this,"exit",Toast.LENGTH_SHORT).show();
+            }
+        });
+        builder.setNeutralButton("cancel", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                Toast.makeText(Ch4Activity4.this,"cancel",Toast.LENGTH_SHORT).show();
+            }
+        });
+         builder.create().show();
 
     }
 }
---
+++ app/src/main/java/cn/edu/sdwu/android/classroom/sn170507180228/Ch4Activity5.java
@@ -0,0 +1,25 @@
+package cn.edu.sdwu.android.classroom.sn170507180228;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+public class Ch4Activity5 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.layout_ch4_6);
+        //在JANA中获取字符串资源
+        String content=getString(R.string.hello);
+        Log.i(Ch4Activity5.this.toString(),content);
+        String sms=getString(R.string.sms);
+        sms=String.format(sms,100,"张三");
+        Log.i(Ch4Activity5.this.toString(),content);
+
+
+
+
+    }
+}
