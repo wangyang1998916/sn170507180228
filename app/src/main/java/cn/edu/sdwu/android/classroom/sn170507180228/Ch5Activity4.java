97f9b43ed2f5439395a4e81d5a6ca0ac33c2ef8a
wangyang1998916 <2931113553@qq.com>
2020-04-16 00:07:30 +0800

    更新

---
+++ app/src/main/java/cn/edu/sdwu/android/classroom/sn170507180228/Ch5Activity4.java
@@ -0,0 +1,15 @@
+package cn.edu.sdwu.android.classroom.sn170507180228;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Ch5Activity4 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.my_ch1_1);
+    }
+
+}
